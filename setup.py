82201dfe50bde043fd897e7779390a0512e05555
Reginald Gouws <reginaldgouws95@gmail.com>
2025-05-27 08:53:25 +0200

    Restore deleted files: modules.txt, setup.py, requirements.txt, .gitignore

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+	install_requires = f.read().strip().split("\n")
+
+# get version from __version__ variable in supplier_feed/__init__.py
+from supplier_feed import __version__ as version
+
+setup(
+	name="supplier_feed",
+	version=version,
+	description="Manage and import supplier feeds in XML, CSV, and JSON format",
+	author="Reginald Gouws",
+	author_email="reginaldgouws95@gmail.com",
+	packages=find_packages(),
+	zip_safe=False,
+	include_package_data=True,
+	install_requires=install_requires
+)
